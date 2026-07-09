c8463f5459791ac10f03f7c9155483c43e848f48
joseagim <joseagim@ucm.es>
2026-07-09 14:07:17 +0200

    SpringBoot & PostgreSQL connected

---
+++ traintracker/src/main/java/com/joseagim/traintracker/TraintrackerApplication.java
@@ -0,0 +1,13 @@
+package com.joseagim.traintracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraintrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraintrackerApplication.class, args);
+    }
+
+}
---
+++ traintracker/src/test/java/com/joseagim/traintracker/TraintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseagim.traintracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraintrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
