4c47795dff8384156ba4c5b42a1d46286b41ffe0
joseagim <joseagim@ucm.es>
2026-07-23 14:29:36 +0200

    Fix: removed bean validation on response dto

--- traintracker/src/main/java/com/joseagim/traintracker/dto/response/StationResponseDto.java
+++ traintracker/src/main/java/com/joseagim/traintracker/dto/response/StationResponseDto.java
@@ -1,10 +1,8 @@
 package com.joseagim.traintracker.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record StationResponseDto(
         Long id,
-        @NotBlank String name,
-        @NotBlank String city
+        String name,
+        String city
 ) {
 }
